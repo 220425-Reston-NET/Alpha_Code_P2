b57b11ed09a4eb9faccc589cfc68eb9236a5616f
psycheisrael <mudyaisrael@gmail.com>
2022-06-21 15:52:28 -0400

    frontend connection

--- MedTrakApi/Program.cs
+++ MedTrakApi/Program.cs
@@ -5,6 +5,18 @@ using MedTrakModel;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Adding CORS to allow all origins to have acess to our backend
+builder.Services.AddCors(
+    (options) => {
+        options.AddDefaultPolicy(origin => {
+
+            origin.AllowAnyOrigin(); //Allows any origin to talk to your backend
+            origin.AllowAnyMethod(); //Allows any http verb request in our backend
+            origin.AllowAnyHeader(); //Allows any http headers to have access to my backend
+        });
+    }
+);
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -24,6 +36,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
